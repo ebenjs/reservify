d75ac2c911e14ca02ea1aa6badc0197632d7d1a7
Ebenezer Nikabou <nikaboue10@gmail.com>
2024-03-23 00:00:42 +0200

    first commit

---
+++ src/main/java/dev/ebenjs/reservify/ReservifyApplication.java
@@ -0,0 +1,13 @@
+package dev.ebenjs.reservify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ebenjs/reservify/ReservifyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ebenjs.reservify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
